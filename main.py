9ff44c8fb338cfe5a16dfd4a5b4c0063fbd1220e
jbillote <jbillote@users.noreply.github.com>
2023-11-17 01:35:01 -0800

    Implement servant detail fetch

--- main.py
+++ main.py
@@ -27,6 +27,45 @@ def servant_search():
 
     return results
 
+@app.route("/api/fgo/v1/servant/<int:id>", methods=["GET"])
+def get_servant_info(id):
+    url = f"https://api.atlasacademy.io/nice/JP/servant/{id}?lang=en"
+    response = requests.get(url)
+    data = response.json()
+
+    portraits = []
+    portraits_response_keys = list(data["extraAssets"]["charaGraph"]["ascension"].keys())
+    portraits_response_keys.sort()
+    for k in portraits_response_keys:
+        portraits.append(data["extraAssets"]["charaGraph"]["ascension"][k])
+
+    skills = []
+    for s in data["skills"]:
+        skills.append({
+            "name": s["name"],
+            "icon": s["icon"]
+        })
+
+    appends = []
+    for a in data["appendPassive"]:
+        appends.append({
+            "name": a["skill"]["name"],
+            "icon": a["skill"]["icon"]
+        })
+
+    return {
+        "id": data["id"],
+        "name": data["name"],
+        "classIcon": f"https://static.atlasacademy.io/JP/ClassIcons/class{class_icon_filename(data["rarity"], data["classId"])}.png",
+        "icon": data["extraAssets"]["faces"]["ascension"]["1"],
+        "portraits": portraits,
+        "skills": skills,
+        "appends": appends,
+        "ascensionMaterials": process_materials(data["ascensionMaterials"]),
+        "skillMaterials": process_materials(data["skillMaterials"]),
+        "appendMaterials": process_materials(data["appendSkillMaterials"])
+    }
+
 def class_icon_filename(rarity, class_id):
     if rarity == 3 or rarity == 2:
         rarity -= 1
@@ -34,3 +73,25 @@ def class_icon_filename(rarity, class_id):
         rarity = min(3, rarity)
 
     return f"{rarity}_{class_id}"
+
+def process_materials(materials):
+    processed = []
+
+    materials_keys = list(materials.keys())
+    materials_keys.sort()
+
+    for k in materials_keys:
+        items = []
+        for i in materials[k]["items"]:
+            items.append({
+                "id": i["item"]["id"],
+                "name": i["item"]["name"],
+                "icon": i["item"]["icon"],
+                "amount": i["amount"]
+            })
+        processed.append({
+            "materials": items,
+            "qp": materials[k]["qp"]
+        })
+    
+    return processed
